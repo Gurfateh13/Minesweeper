bbcb30b7d7e785759325fb2fd37a73ba50b4f87c
Gurfateh13 <57686511+Gurfateh13@users.noreply.github.com>
2019-12-03 11:03:56 -0500

    Update MineSweeperWindow.java
    made window bigger

--- MineSweeperWindow.java
+++ MineSweeperWindow.java
@@ -6,7 +6,7 @@ public class MineSweeperWindow extends JFrame{
   public static void main (String args[]) {
     MineSweeperEasyWind gui1 = new MineSweeperEasyWind();
     gui1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    gui1.setSize(400,500);
+    gui1.setSize(600,640);
     gui1.setVisible(true);
     gui1.setTitle("MineSweeper");
     
