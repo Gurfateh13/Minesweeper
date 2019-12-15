df05f9af7b57b9f2172ffd660a3355e24a1caa5d
rkhan5380 <57686501+rkhan5380@users.noreply.github.com>
2019-12-15 17:24:39 -0500

    Update MineSweeperWindow.java
    this is the file that the user should run

--- MineSweeperWindow.java
+++ MineSweeperWindow.java
@@ -1,11 +1,11 @@
+//adds the required packages
 import javax.swing.*;
 import java.awt.*;
 import java.lang.*;
-
 import java.awt.event.*;
-
+//MineSweeperWindow is a child of JFrame
 public class MineSweeperWindow extends JFrame{
-  
+//main method  
   public static void main (String args[]) {
     MineSweeperHomeWindow gui1 = new MineSweeperHomeWindow();
     gui1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
